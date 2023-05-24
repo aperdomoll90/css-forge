c4951eb4a2e3e532aee1de77a49bcf544c06a322
jordanaman <jordanaman127@gmail.com>
2023-05-23 21:52:21 -0400

    change control, remove dom selectors

--- src/ToggleButton/ToggleButton.tsx
+++ src/ToggleButton/ToggleButton.tsx
@@ -1,42 +1,62 @@
-import { useEffect } from 'react'
-import './styles.css'
-import '../utils/GlobalStyles.css'
-import { ToggleButtonPropsType } from './ToggleButton.types'
-import { LightenDarkenColor } from '../utils/ColorManipulation'
+import "./styles.css";
+import "../utils/GlobalStyles.css";
+import { ToggleButtonPropsType } from "./ToggleButton.types";
+import { LightenDarkenColor } from "../utils/ColorManipulation";
 
-export const ToggleButton: React.FC<ToggleButtonPropsType> = ({ size, color, buttonHover, buttonBackgroundColor, shadow, setActive, ariaControls, ariaExpanded, top, bottom, left, right, customClass }) => {
-  const defaultColor = '#fff'
-  const defaultBackgroundColor = '#303030da'
+export const ToggleButton: React.FC<ToggleButtonPropsType> = ({
+  size,
+  color,
+  buttonHover,
+  buttonBackgroundColor,
+  shadow,
+  active,
+  setActive,
+  ariaControls,
+  ariaExpanded,
+  top,
+  bottom,
+  left,
+  right,
+  customClass,
+}) => {
+  const defaultColor = "#fff";
+  const defaultBackgroundColor = "#303030da";
 
   const stylesProps = {
-    '--size': size ? `${size}rem` : '2rem',
-    '--color': color ? color : defaultColor,
-    '--buttonHover': buttonHover ? buttonHover : LightenDarkenColor(buttonBackgroundColor || defaultBackgroundColor, -80),
-    '--buttonBackgroundColor': buttonBackgroundColor ? buttonBackgroundColor : defaultBackgroundColor,
-    '--shadow': shadow ? 'rgba(0, 0, 0, 0.8)' : '',
-    '--top': top ? `${top}rem` : 'auto',
-    '--bottom': bottom ? `${bottom}rem` : 'auto',
-    '--left': left ? `${left}rem` : 'auto',
-    '--right': right ? `${right}rem` : 'auto',
-  }
-  const toggleMenu = () => setActive((value: Boolean) => !value)
+    "--size": size ? `${size}rem` : "2rem",
+    "--color": color ? color : defaultColor,
+    "--buttonHover": buttonHover
+      ? buttonHover
+      : LightenDarkenColor(
+          buttonBackgroundColor || defaultBackgroundColor,
+          -80
+        ),
+    "--buttonBackgroundColor": buttonBackgroundColor
+      ? buttonBackgroundColor
+      : defaultBackgroundColor,
+    "--shadow": shadow ? "rgba(0, 0, 0, 0.8)" : "",
+    "--top": top ? `${top}rem` : "auto",
+    "--bottom": bottom ? `${bottom}rem` : "auto",
+    "--left": left ? `${left}rem` : "auto",
+    "--right": right ? `${right}rem` : "auto",
+  };
 
-  useEffect(() => {
-    let menuToggle = document.querySelector('.ToggleButton') as HTMLElement
-    if (menuToggle) {
-      menuToggle.onclick = function () {
-        menuToggle.classList.toggle('ToggleButtonActive')
-        toggleMenu()
-      }
-    }
-  }, [])
-  const classArray = customClass && customClass ? `ToggleButton ${customClass}` : 'ToggleButton'
+  const isActive = active ? "ToggleButtonActive ToggleButton" : "ToggleButton";
+  const classArray =
+    customClass && customClass ? `${isActive} ${customClass}` : isActive;
+
   return (
-    <button className={classArray} aria-controls={ariaControls} aria-expanded={ariaExpanded} style={stylesProps as React.CSSProperties}>
+    <button
+      className={classArray}
+      aria-controls={ariaControls}
+      aria-expanded={ariaExpanded}
+      style={stylesProps as React.CSSProperties}
+      onClick={() => setActive()}
+    >
       <span></span>
       <span></span>
       <span></span>
-      <span className='sr-only'>menu</span>
+      <span className="sr-only">menu</span>
     </button>
-  )
-}
+  );
+};
